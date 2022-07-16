0dc54314a4ac3fa7487102efa84ab4e56a0969a0
João Victor <jvbaprof@gmail.com>
2022-07-16 11:48:49 -0300

    Format changes

--- src/components/Botao.tsx
+++ src/components/Botao.tsx
@@ -11,13 +11,18 @@ export default function Botao(props: BotaoProps) {
     //bg-gradient-to-r from-${props.cor}-400 to-${props.cor}-700
     //bg-gradient-to-r from-green-400 to-green-700
     return (
-        <button onClick={props.onClick} className={cor === 'green' ? `
-            bg-gradient-to-r from-green-400 to-green-700
+        <button onClick={props.onClick} className=
+        {cor === 'green' ? 
+            `bg-gradient-to-r from-green-400 to-green-700
             text-white px-4 py-2 rounded-md
-            ${props.className}
-        ` : cor === 'gray' ? `bg-gradient-to-r from-gray-400 to-gray-700
+            ${props.className}` 
+
+        : cor === 'gray' ? 
+        `bg-gradient-to-r from-gray-400 to-gray-700
         text-white px-4 py-2 rounded-md
-        ${props.className}` : `bg-gradient-to-r from-yellow-400 to-yellow-700
+        ${props.className}` 
+
+        : `bg-gradient-to-r from-yellow-400 to-yellow-700
         text-white px-4 py-2 rounded-md
         ${props.className}`}>
             {props.children}
